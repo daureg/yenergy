bfa7aaefb93eec0ce5e1551a645300c8cee366c6
Géraud Le Falher <daureg@gmail.com>
2014-05-30 09:56:16 +0300

    display more info

--- data/buildings_info.js
+++ data/buildings_info.js
@@ -1,49 +1,49 @@
 var INFO = {
-    "way/4252946":  {"elec":3200.02270663,    "heat":21223.9819159,    "surface":41339},
-    "way/7588374":  {"elec":558.784783896,    "heat":11746.985663,    "surface":11202},
-    "way/7584131":  {"elec":1608.11902655,    "heat":1941.62479339,    "surface":3770},
-    "way/4217650":  {"elec":2202.94930477,    "heat":13150.8846736,  "surface":13058,
-        comments:[{date: "2014-05-21", author: "Tom", msg: "I'm pretty sure that all those computers are crunching numbers to save the planet at some point."}]},
-    "way/7588741":  {"elec":147.10031348,    "heat":0.0,    "surface":469},
-    "way/27930945": {"elec":1813.51110326,    "heat":7672.53755548,   "surface":12774},
-    "way/26608177": {"elec":1398.7195122,    "heat":2700.31796502,    "surface":3736},
-    "way/4252948":  {"elec":6974.6978852,    "heat":8880.87248322,   "surface":7808},
-    "way/7578961":  {"elec":246.706258816,    "heat":1923.33288273,    "surface":3505},
-    "way/7244238":  {"elec":1718.24546952,   "heat":5966.69329073, "surface":6359},
-    "way/7579330":  {"elec":4126.41281823,    "heat":3899.76038339,   "surface":5868},
-    "way/26608273": {"elec":4187.58183306,    "heat":11021.2370778,  "surface":11697},
-    "way/26608317": {"elec":1275.48622366,    "heat":4787.21682848, "surface":5100},
-    "way/26608330": {"elec":3943.42857143,    "heat":0.0,   "surface":8766},
-    "way/7584026":  {"elec":781.466227348,    "heat":1457.89288775,    "surface":3268},
-    "way/156903276":{"elec":1138.77295492,    "heat":3042.8802589,   "surface":5544},
-    "way/156903284":{"elec":320.945561139,    "heat":0.0,  "surface":2005},
-    "way/7588058":  {"elec":4942.40400668,    "heat":0.0,    "surface":5100},
-    "way/24342454": {"elec":4304.05458861,    "heat":24454.3434555,  "surface":16245},
-    "way/29012757": {"elec":6386.47476341,    "heat":29354.7115325,  "surface":26260},
-    "way/4259025":  {"elec":1324.1308582,    "heat":2716.52892562,   "surface":8206},
-    "way/27464085": {"elec":5611.17647059,    "heat":10027.5387263,   "surface":18043},
-    "way/26608151": {"elec":344.710355987, "heat":882.200647249,    "surface":1406},
+	"way/7588374": {"elec": 558.784783896, "comments": [], "source": "nuclear",
+		"heat": 11746.985663, "surface": 11202, "year": 1973},
+	"way/156903284": {"elec": 320.945561139, "comments": [], "source": "solar",
+		"heat": 0.0, "surface": 2005, "year": 1975},
+	"way/7578961": {"elec": 246.706258816, "comments": [], "source": "wind",
+		"heat": 1923.33288273, "surface": 3505, "year": 1989},
+	"way/26608273": {"elec": 4187.58183306, "comments": [], "source": "solar",
+		"heat": 11021.2370778, "surface": 11697, "year": 1986},
+	"way/27930945": {"elec": 1813.51110326, "comments": [], "source": "wind",
+		"heat": 7672.53755548, "surface": 12774, "year": 1981},
+	"way/7588741": {"elec": 147.10031348, "comments": [], "source": "wind",
+		"heat": 0.0, "surface": 469, "year": 1972},
+	"way/4259025": {"elec": 1324.1308582, "comments": [], "source": "nuclear",
+		"heat": 2716.52892562, "surface": 8206, "year": 1992},
+	"way/4252948": {"elec": 6974.6978852, "comments": [], "source": "wind",
+		"heat": 8880.87248322, "surface": 7808, "year": 1997},
+	"way/26608330": {"elec": 3943.42857143, "comments": [], "source": "nuclear",
+		"heat": 0.0, "surface": 8766, "year": 1986},
+	"way/26608317": {"elec": 1275.48622366, "comments": [], "source": "wind",
+		"heat": 4787.21682848, "surface": 5100, "year": 1987},
+	"way/156903276": {"elec": 1138.77295492, "comments": [], "source": "nuclear",
+		"heat": 3042.8802589, "surface": 5544, "year": 1989},
+	"way/7584026": {"elec": 781.466227348, "comments": [], "source": "wind",
+		"heat": 1457.89288775, "surface": 3268, "year": 1992},
+	"way/7588058": {"elec": 4942.40400668, "comments": [], "source": "wind",
+		"heat": 0.0, "surface": 5100, "year": 1983},
+	"way/7244238": {"elec": 1718.24546952, "comments": [], "source": "solar",
+		"heat": 5966.69329073, "surface": 6359, "year": 1988},
+	"way/24342454": {"elec": 4304.05458861, "comments": [], "source": "nuclear",
+		"heat": 24454.3434555, "surface": 16245, "year": 1998},
+	"way/4252946": {"elec": 3200.02270663, "comments": [], "source": "solar",
+		"heat": 21223.9819159, "surface": 41339, "year": 1988},
+	"way/26608177": {"elec": 1398.7195122, "comments": [], "source": "wind",
+		"heat": 2700.31796502, "surface": 3736, "year": 1970},
+	"way/29012757": {"elec": 6386.47476341, "comments": [], "source": "nuclear",
+		"heat": 29354.7115325, "surface": 26260, "year": 2000},
+	"way/27464085": {"elec": 5611.17647059, "comments": [], "source": "wind",
+		"heat": 10027.5387263, "surface": 18043, "year": 1972},
+	"way/4217650": {"elec": 2202.94930477, "comments": [
+		{"date": "2014-05-17", "author": "John", "msg": "The temperature there is really good."},
+		{"date": "2014-05-21", "msg": "I'm pretty sure that all those computers are crunching numbers to save the planet at some point.", "author": "Tom"}], "source": "nuclear", "heat": 13150.8846736, "surface": 13058, "year": 1975},
+	"way/26608151": {"elec": 344.710355987, "comments": [], "source": "solar",
+		"heat": 882.200647249, "surface": 1406, "year": 1978},
+	"way/7579330": {"elec": 4126.41281823, "comments": [], "source": "nuclear",
+		"heat": 3899.76038339, "surface": 5868, "year": 1993},
+	"way/7584131": {"elec": 1608.11902655, "comments": [], "source": "nuclear",
+		"heat": 1941.62479339, "surface": 3770, "year": 1983}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- static/display.js
+++ static/display.js
@@ -9,13 +9,20 @@ var popup_content = '<h2>{{name}}</h2>';
 popup_content += '<p>Electricity: {{elec}} kWh<br>';
 popup_content += 'Heat: {{heat}} kWh</p>';
 
-var info_content = '<h2>{{name}}</h2>';
-info_content += '<p>Electricity: {{elec}} kWh<br>';
-info_content += 'Heat: {{heat}} kWh<br>';
-info_content += 'Surface: {{surface}} m²</p><h3>Air quality</h3><div id="graph"></div>';
+var info_content = '<header style="background: {{color}};">';
+info_content += '<img id="close" class="icon close" src="static/images/close.svg"><h2>{{name}}</h2></header>';
+info_content += '<h3>Consumption <img  class="icon" src="static/images/{{source}}.svg" alt="{{source}} energy"></h3>';
+info_content += '<table><tr><td>Electricity</td><td>{{elec}} kWh</td></tr>';
+info_content += '<tr><td>Heat</td><td>{{heat}} kWh</td></tr></table>';
+info_content += '</p><h3>Air quality';
+info_content += '<small> in the last hour</small></h3><div id="graph"></div>';
+info_content += '<h3>Other informations</h3>';
+info_content += '<table><tr><td>Age</td><td>{{age}} years</td></tr>';
+info_content += '<tr><td>Surface</td><td>{{surface}} m²</td></tr></table>';
+var COMMENT_FMT = '<li><blockquote>{{msg}}<br><emph>{{author}}</emph>—{{date}}</blockquote></li>';
 var MINI = require('minified');
 var _=MINI._, $=MINI.$, $$=MINI.$$, EE=MINI.EE, HTML=MINI.HTML;
-/*
+var COLORS = ['firebrick', 'darkorange', 'yellowgreen', 'lime'];
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(60.185, 24.827),
@@ -27,24 +34,21 @@ function initialize() {
     map.data.setStyle(function(feature) {
         var id = feature.getProperty('@id');
         var surface = INFO[id].surface;
-        feature.setProperty('surface', surface);
         var elec = INFO[id].elec;
-        feature.setProperty('elec', elec);
-        var heat = INFO[id].heat;
-        feature.setProperty('heat', heat);
-        var comments = INFO[id].comments;
-        feature.setProperty('comments', comments);
         var eff = parseFloat(elec)/parseFloat(surface);
-        var color =  eff > 0.25 ? 'red' : 'green';
-        if (eff > 0.40) {color = 'red';}
-        if (eff < 0.40) {color = 'darkorange';}
-        if (eff < 0.250) {color = 'green';}
-        if (eff < 0.162) {color = 'limegreen';}
+        var color =  0;
+        if (eff > 0.40) {color = 0;}
+        if (eff < 0.40) {color = 1;}
+        if (eff < 0.250) {color = 2;}
+        if (eff < 0.162) {color = 3;}
+        feature.setProperty('efficiency', color);
+        color = COLORS[color];
         return { fillColor: color, strokeWeight: 1, strokeColor: 'gray',
             fillOpacity: 0.5};
     });
     map.data.addListener('mouseover', function(event) {
         map.data.revertStyle();
+        console.log(event.feature.getProperty('efficiency'));
         var popup = $('#popup');
         if (popup.get('$$show') === 0 || popup.get('$$fade') === 0) {
             popup.set({$left: event.Va.clientX + 10+'px', $top: event.Va.clientY + 10+'px'});
@@ -65,24 +69,27 @@ function initialize() {
         var infoPane = $("#info");
         var data = get_data(event.feature);
         var fmt = info_content;
-        if (event.feature.getProperty('comments')) {
-            fmt += "<h3>1 Review</h3>";
-            fmt += "<blockquote><p>{{msg}}<br><emph>{{author}}</emph>—{{date}}</p></blockquote>";
-            _.extend(data, event.feature.getProperty('comments')[0]);
+        if (data.nb_reviews > 0) {
+            fmt += "<h3>{{nb_reviews}} Reviews</h3><ol>";
+            for (var i = 0; i < data.nb_reviews; i++) {
+                fmt += _.format(COMMENT_FMT, data.comments[i]);
+            }
+            fmt += "</ol>";
         }
         infoPane.fill(HTML(fmt, data));
+        $('#close').onClick(closePanel);
         var now = Date.create();
         var before = Date.create().rewind({ minutes: 59 });
         graph = create_graph('graph');
-	plot(graph, fake.r);
-        $.request('get', '/air/'+date_to_str(before)+'/'+date_to_str(now))
-        .then(function(txt) {
-            var json = $.parseJSON(txt);
-            plot(graph, json.r);
-        })
-        .error(function(status, statusText, responseText) {
-            console.log(status, statusText, responseText);
-        });
+        plot(graph, fake.r);
+        // $.request('get', '/air/'+date_to_str(before)+'/'+date_to_str(now))
+        // .then(function(txt) {
+        //     var json = $.parseJSON(txt);
+        //     plot(graph, json.r);
+        // })
+        // .error(function(status, statusText, responseText) {
+        //     console.log(status, statusText, responseText);
+        // });
         if (!infoOpen) {
         $('#popup').animate({$$fade: 0}, anim_ms);
         mapSlide();
@@ -91,64 +98,32 @@ function initialize() {
         infoOpen = true;
     });
 }
-*/
-// google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
 
 function get_data(feature) {
-    return  {name: feature.getProperty('int_name'),
-        heat: parseFloat(feature.getProperty('heat')).format(2, ' ', '.'),
-        elec: parseFloat(feature.getProperty('elec')).format(2, ' ', '.'),
-        surface: feature.getProperty('surface')};
+    // clone the object
+    var building = JSON.parse(JSON.stringify(INFO[feature.getProperty('@id')]));
+    _.extend(building, {name: feature.getProperty('int_name'),
+        heat: parseFloat(building.heat).format(2, ' ', '.'),
+        elec: parseFloat(building.elec).format(2, ' ', '.'),
+        nb_reviews: building.comments.length,
+        age: (new Date()).getFullYear() - building.year,
+        color: COLORS[feature.getProperty('efficiency')]
+    });
+    return building;
 }
 var mapSlide = null,
     infoSlide = null,
     infoOpen = false,
-    anim_ms = 20;
+    anim_ms = 200;
 
-var colors = ['red', 'darkorange', 'green', 'limegreen'];
 $(function() {
-    mapSlide = $('#map-canvas').toggle({$left: '0'}, {$left: '-25%'}, anim_ms);
+    mapSlide = $('#map-canvas').toggle({$left: '0'}, {$left: '-34%'}, anim_ms);
     infoSlide = $('#info').toggle({$left: '100%', $display: 'none'},
         {$left: '66%', $display: 'block'}, anim_ms);
-
     document.addEventListener('keydown', function(event) {
         if (event.keyCode === 27) { closePanel(); }
     }, false);
-    /* Mockup */
-    var CS = {"elec":2202.94,   "heat":13150.88,  "surface":13058,
-	    eff: 0, sources: 'wind', year: 1985, name: "Aalto Computer Science",
-	comments:[{date: "2014-05-17", author: "John", msg: "The temperature there is really good."},
-    {date: "2014-05-21", author: "Tom",
-		msg: "I'm pretty sure that all those computers are crunching numbers to save the planet at some point."}]};
-    var infoPane = $("#info");
-    infoSlide();
-    infoOpen = true;
-    var info_content = '<header style="background: {{color}};">';
-    info_content += '<img class="icon close" src="static/images/close.svg"><h2>{{name}}</h2></header>';
-    info_content += '<h3>Consumption <img  class="icon" src="static/images/{{sources}}.svg" alt="{{sources}} energy"></h3>';
-    info_content += '<table><tr><td>Electricity</td><td>{{elec}} kWh</td></tr>';
-    info_content += '<tr><td>Heat</td><td>{{heat}} kWh</td></tr></table>';
-    info_content += '</p><h3>Air quality';
-    info_content += '<small> in the last hour</small></h3><div id="graph"></div>';
-    info_content += '<h3>Other informations</h3>';
-    info_content += '<table><tr><td>Age</td><td>{{age}} years</td></tr>';
-    info_content += '<tr><td>Surface</td><td>{{surface}} m²</td></tr></table>';
-    var nb_reviews = CS.comments.length;
-    if (nb_reviews > 0) {
-	    info_content += "<h3>{{nb_reviews}} Reviews</h3><ol>";
-	    for (var i = 0; i < nb_reviews; i++) {
-		    info_content += _.format("<li><blockquote>{{msg}}<br><emph>{{author}}</emph>—{{date}}</blockquote></li>",
-				    CS.comments[i]);
-	    }
-        info_content += "</ol>";
-    }
-    var fmt = info_content;
-    var data = CS;
-    _.extend(data, {nb_reviews: nb_reviews, age: 2014 - CS.year, color: colors[CS.eff]});
-    infoPane.fill(HTML(fmt, data));
-        graph = create_graph('graph');
-	plot(graph, fake.r);
-
 });
 function closePanel() {
 	mapSlide();
